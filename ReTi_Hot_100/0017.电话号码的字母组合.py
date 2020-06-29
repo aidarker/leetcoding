7f7514302b9a7bf8a605d0c9a77e103ca791b8b8
aidarker <koukai6713@gmail.com>
2020-06-29 19:23:52 +0800

    17

--- ReTi_Hot_100/0017.电话号码的字母组合.py
+++ ReTi_Hot_100/0017.电话号码的字母组合.py
@@ -18,6 +18,7 @@ sys.path.append(path)
 import time
 import datetime
 import logging
+import numpy as np
 logging.basicConfig(level = logging.INFO,format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
@@ -28,13 +29,38 @@ class Solution(object):
         :type digits: str
         :rtype: List[str]
         """
-        print(digits)
+
+        dic = {
+                '2': ['a', 'b', 'c'],
+                '3': ['d', 'e', 'f'],
+                '4': ['g', 'h', 'i'],
+                '5':['j', 'k', 'l'],
+                '6':['m', 'n', 'o'],
+                '7':['p', 'q', 'r', 's'],
+                '8':['t', 'u', 'v'],
+                '9':['w', 'x', 'y', 'z']
+            }
+        
+        arr = dic[digits[0]]
+        t = []
+        for i in digits[1:]:
+            for j in arr:
+                for k in dic[i]:
+                    t.append(j+k)
+            arr = t.copy()
+        
+        n = len(digits)
+        t1 = []
+        for k in arr:
+            if len(k) == n:
+                t1.append(k)
+        return t1
 
 
 if __name__ == '__main__':
     start = time.time()
     
-    digits = "23"
+    digits = "2345"
     slc = Solution()
     slc.letterCombinations(digits)
     
