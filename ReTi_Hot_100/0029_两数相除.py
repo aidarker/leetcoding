8fe858469ae66158dc6b8ad242c67c99a3a96f38
koukai <koukai@autohome.com.cn>
2020-09-11 14:47:48 +0800

    热题29

--- ReTi_Hot_100/0029_两数相除.py
+++ ReTi_Hot_100/0029_两数相除.py
@@ -17,38 +17,19 @@ sys.path.append(path)
 
 import time
 import datetime
-import logging
-
 
 class Solution(object):
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        if not needle:
-            return 0
+    def divide(self, dividend, divisor):
+        if divisor == 0:
+            return
         
-        if not haystack:
-            return -1
-
-        if needle in haystack:
-            return haystack.index(needle)
-        else:
-            return -1
 
 
 if __name__ == '__main__':
     start = time.time()
-    
-    sss = Solution()
 
-    haystack = "hello"
-    needle = "ll"
-    result = sss.strStr(haystack, needle)
-    print(result)
 
     end = time.time()
     delta = end - start
     print("Finished time is {}".format(datetime.timedelta(seconds=delta)))
+
