45f6f195a69c7489c090effa77537e00909139d5
koukai <koukai@autohome.com.cn>
2020-10-24 13:41:48 +0800

    0034

--- ReTi_Hot_100/0034.在排序数组中查找元素的第一个和最后一个位置.py
+++ ReTi_Hot_100/0034.在排序数组中查找元素的第一个和最后一个位置.py
@@ -2,8 +2,8 @@
 # -*- encoding: utf-8 -*-
 
 '''
-@File    :   0034.在排序数组中查找元素的第一个和最后一个位置.py
-@Time    :   2020-10-24
+@File    :   0034. 在排序数组中查找元素的第一个和最后一个位置.py
+@Time    :   2020-10-14
 @Author  :   KouKai
 @Version :   V1.0
 @Remarks :   None
@@ -20,22 +20,30 @@ import datetime
 
 class Solution(object):
     def searchRange(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
+        """ """
+        low, high = 0, len(nums)
+        while(low<high):
+            mid = (low + high) // 2
+            mid_value = nums[mid]
+            if target == mid_value:
+                return [mid, mid]
+
+            if mid_value > target:
+                high = mid
+            else:
+                low = mid + 1
+                
+        return [-1, -1]
 
 
 if __name__ == '__main__':
     start = time.time()
+    
     ssr = Solution()
-
     nums = [5,7,7,8,8,10]
     target = 8
     ssr.searchRange(nums, target)
-
+    
     end = time.time()
     delta = end - start
     print("Finished time is {}".format(datetime.timedelta(seconds=delta)))
